c39d6de2555f5e49643868dbfc2491b956768bb7
wande <yewandeadeyemi84@gmail.com>
2023-10-03 22:47:32 +0100

    another commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const words = ["Men","Women","Kids","Yourself"];
+
+let currentWordIndex = 0;
+const textElement =  document.getElementById("rotatingHeading");
+
+function rotateWord(){
+   textElement.textContent = words[currentWordIndex];
+   currentWordIndex = (currentWordIndex + 1) % words.length;
+}
+
+rotateWord();
+setInterval(rotateWord, 2000);
