9ff2aa0e639eb840fd04b7164f35f511dc8e0063
wande <yewandeadeyemi84@gmail.com>
2023-12-03 22:44:17 +0100

    updated

--- index.js
+++ index.js
@@ -12,5 +12,28 @@ rotateWord() ;
 setInterval(rotateWord, 2000);
 
 
-
+// carousel
+$('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:10,
+   dots: false,
+   autoplay:true,
+   autoplayTimeout: 3000,
+   autoplayHoverPause: true,
+   responsiveClass:true,
+   responsive:{
+       0:{
+           items:1,
+           nav:true
+       },
+       600:{
+           items:1,
+           nav:true
+       },
+       1000:{
+        items:3,
+        nav:true
+    }
+   }
+})
 
