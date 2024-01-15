733a53c0a70474ca55ea198a54a75007a1b43270
wande <yewandeadeyemi84@gmail.com>
2024-01-15 23:39:52 +0100

    added productlist dropdown

--- index.js
+++ index.js
@@ -48,24 +48,67 @@ breakpoints: {
     // },
   });
 
+
+// // to pick a mother dropdown
+// function motherDrop(dropNum){
+//   let dropdowns = document.getElementsByClassName("product-dropdown")
+//   for (let i = 0; i < dropdowns.length; i++) {
+//     if (i + 1 !== dropNum) {
+//       dropdowns[i].style.display = "none";
+//     }
+//   }
+//   let currentDropdown = document.getElementsByClassName("product-dropdown" + dropNum);
+//   if (currentDropdown) {
+//     for (let i = 0; i < dropdowns.length; i++) {
+//       let currentDropdown = dropdowns[i];
+//       // Toggle the display property
+//       if (currentDropdown.style.display === "none" || currentDropdown.style.display === "") {
+//         currentDropdown.style.display = "block";
+//       } else {
+//         currentDropdown.style.display = "none";
+//       }
+//     }
+ 
+//   }
+// }
+
+
+
+
   // mother element dropdown
 const dropdownBtn = document.getElementById('imgbtn');
-const dropdownContent = document.getElementById('dropdown');
+const dropdownContent = document.getElementById("dropdown");
 
 // children element dropdown
 const productDropdown = document.querySelectorAll('.flexArr');
 const productDrop = document.querySelector('.product-dropdown');
+// men product dropdown
+const menUndies = document.getElementById('imgbtn1');
+const menUndiesDropdown = document.querySelector('.product-dropdown1');
+// women product dropdown
+const womenUndies = document.getElementById('imgbtn2');
+const womenUndiesDropdown = document.querySelector('.product-dropdown2');
+
 
 
 
 dropdownBtn.addEventListener('click', function(){
   dropdownContent.classList.toggle('show')
   productDrop.classList.remove('show');
+
 })
+
 productDropdown.forEach(function(element) {
   // Add a click event listener to each element
   element.addEventListener('click', function() {
     // Toggle the class 'show' on productDrop
     productDrop.classList.toggle('show');
   });
+});
+
+menUndies.addEventListener('click', function(){
+menUndiesDropdown.classList.toggle('show')
+});
+womenUndies.addEventListener('click', function(){
+womenUndiesDropdown.classList.toggle('show')
 });
