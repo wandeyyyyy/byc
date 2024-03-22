97076ee13774e0b8f2530b54b57ba859dd8acc1d
wande <yewandeadeyemi84@gmail.com>
2024-03-22 16:13:34 +0100

    product page

--- api.js
+++ api.js
@@ -141,3 +141,63 @@ Swal.fire({
 
 
 
+
+
+      function getProducts(){
+        const mentos = document.querySelector(".mentos");
+      
+      
+      
+      
+        const url = "http://localhost:4000/api/products";
+      
+        const methodProduct = {
+          method: "GET",
+        };
+        
+      let data = []
+        fetch(url, methodProduct)
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result);
+      
+            
+            result.map((item) => {
+              
+      
+              data += `
+              
+              <div class="col-sm-12 col-md-6 col-lg-4  mt-5">
+        
+                <img src="${item.image[0]}" alt=""  class="productImg">
+              
+                    <h6 class="mt-3">${item.name}</h6>
+                    <span class="byc">${item.code}</span>
+                    <p class="fashion mt-3">${item.description}</p>
+                    <p class="price">$${item.price}</p>
+                    <div class="starNum">
+                    <div class="star">
+                   
+                      <span><img src="assets/star.jpg" alt=""></span>
+                      <span><img src="assets/star.jpg" alt=""></span>
+                      <span><img src="assets/star.jpg" alt=""></span>
+                      <span><img src="assets/star.jpg" alt=""></span>
+                      <span><img src="assets/halfstar.png" alt=""></span>
+        
+                    </div>
+                    <div class="rate"><p>4.05</p></div>
+                    </div>
+                    <div class="d-flex mt-3">
+                        <button class="btn-wish me-1"><i class="fa-regular fa-heart me-2" style="color: #BD3A3A;"></i>Wishlist</button>
+                        <button class="btn-shoppingcart ml-1"><i class="fa-solid fa-cart-shopping me-2" style="color: #BD3A3A;"></i>Buy Now</button>
+                        </div>
+                    </div>  `
+                    mentos.innerHTML = data;
+            });
+          
+           
+          });
+          
+      }
+
+       
